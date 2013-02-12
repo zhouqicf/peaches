2116eaa74b678011d5aca07bbf4585a11207d606
sliuqin@gmail.com <quyd8spal2te4to>
2013-02-12 20:26:38 +0800

    pom getPropertyValue

--- test/pom.js
+++ test/pom.js
@@ -1,18 +1,30 @@
 /**
  * Module dependencies.
  */
-require = require('./testutils');
-
-var POM = require('../lib/pom/index.js'),
-    fs = require('fs'),
+require('should');
+var fs = require('fs'),
     path = require('path'),
     read = fs.readFileSync,
     readdir = fs.readdirSync;
 
+require = require('./testutils');
+var POM = require('../lib/pom/index.js');
+
+describe('POM', function () {
+    'use strict';
+    it('should getPropertyValue ', function () {
+        var pom = new POM('body{padding:10px;}');
+        pom.getPropertyValue('body', 'padding').should.be.an.instanceOf(Array);
+        pom.getPropertyValue('body', 'padding').length.should.eql(1);
+        pom.getPropertyValue('body', 'padding')[0].should.eql('10px');
+    });
+});
 describe('parse(str)', function () {
     'use strict';
     readdir(path.join(__dirname, './style/pom/')).forEach(function (file) {
-        if (~file.indexOf('json')) return;
+        if (file.indexOf('json') > -1) {
+            return;
+        }
         file = path.basename(file, '.css');
         it('should parse ' + file, function () {
             var css = read(path.join(__dirname, 'style/pom/', file + '.css'), 'utf8');
