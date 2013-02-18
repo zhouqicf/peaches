4d1fdf07287c8ab86ceb22d34d80be679ac15e1b
sliuqin@gmail.com <quyd8spal2te4to>
2013-02-18 14:35:51 +0800

    debug 专用

--- debug.js
+++ debug.js
@@ -2,12 +2,30 @@ var http = require('http');
 var Mocha = require('Mocha');
 var POM = require('./lib/pom');
 var fs = require('fs');
-// Create an HTTP server
+var peaches = require('./lib/peaches');
+var cssbeautify = require('cssbeautify');
+var config = {
+    "sort": "h",
+    "format": "png8",
+    "autoReload": false,
+    "model": "local",
+    "server": {
+        "name": "local",
+        "port": 8099,
+        "root": "/Users/liuqin/.peaches/images",
+        "tmp": "/Users/liuqin/.peaches/tmp",
+        "baseURI": "http://127.0.0.1:8099/"
+    }
+};
+
 var srv = http.createServer(function (req, res) {
+    'use strict';
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var css = fs.readFileSync('./test/style/pom/background.css').toString();
-    var pom = new POM(css);
-    res.end(pom.toString());
+    var css = fs.readFileSync('./test/style/peaches.css').toString();
+    peaches(css, config, function (err, styleText) {
+        res.end(cssbeautify(styleText.toString()));
+    },'test');
 });
 // now that server is running
 srv.listen(1337, '127.0.0.1');
+
