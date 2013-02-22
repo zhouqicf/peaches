395979ae866fb7022a8d11405167ba99ff88084d
sliuqin@gmail.com <quyd8spal2te4to>
2013-02-22 13:46:35 +0800

    update

---
+++ test/tools.js
@@ -0,0 +1,11 @@
+require('should');
+require = require('./testutils');
+var tools = require('../lib/tools');
+describe('POM', function () {
+    'use strict';
+    it('should trimAll ', function () {
+        tools.trimAll(' x ').should.eql('x');
+        tools.trimAll(' x y ').should.eql('x y');
+        tools.trimAll(' x y  z ').should.eql('x y z');
+    });
+});
