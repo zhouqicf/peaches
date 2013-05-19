83b1935b9675875a798208e7d56ceb78d1265956
sliuqin <sliuqin@gmail.com>
2013-05-19 16:26:06 +0800

    #19 API 调用时，禁用logger

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var Peaches = require('./lib/peaches');
 Peaches.concat = require('./lib/concat');
 
+var logger = require('colorful').logging;
+logger.config({
+    level: "log"
+});
 require('./bin/init.js').initPeachesHome();
 
 module.exports = Peaches;
