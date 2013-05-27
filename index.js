46573c0f532e9387f48b94921353063dce704569
sliuqin <sliuqin@gmail.com>
2013-05-27 18:38:45 +0800

    #19 添加peaches.version api 调用时，不显示日志

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ Peaches.concat = require('./lib/concat');
 
 var logger = require('colorful').logging;
 logger.config({
-    level: "log"
+    level: "error"
 });
 require('./bin/init.js').initPeachesHome();
+
+var version = require('./package.json').version;
+Peaches.version = version;
 
 module.exports = Peaches;
